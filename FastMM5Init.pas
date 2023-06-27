9c1814e6e223c7b6a8fbedb71b51bc8ec5675b47
Arman-PC <Arman-PC@ARMAN-DESKTOP>
2023-06-27 06:40:26 +0330

    add init file

---
+++ FastMM5Init.pas
@@ -0,0 +1,30 @@
+unit FastMM5Init;
+
+interface
+
+uses
+  FastMM5;
+
+implementation
+
+initialization
+  FastMM_Initialize;
+
+  if FastMM_ShareMemoryManager then
+  begin
+    if FastMM_LoadDebugSupportLibrary then
+    begin
+      FastMM_EnterDebugMode;
+      
+	  FastMM_MessageBoxEvents := FastMM_MessageBoxEvents
+	  + [mmetUnexpectedMemoryLeakDetail];
+    end;
+  end
+  else
+  begin
+    FastMM_AttemptToUseSharedMemoryManager;
+  end;
+  
+finalization
+  FastMM_ExitDebugMode
+  FastMM_Finalize;
